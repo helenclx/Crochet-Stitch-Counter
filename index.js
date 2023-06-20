67a8ec3066d72bd162f0b02f298e8fa0b5073bd4
Helen Chong <119173961+helenclx@users.noreply.github.com>
2023-06-20 11:28:14 +0800

    Updated app

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+const countEl = document.querySelector(".count");
+const prevCountEl = document.querySelector(".prev-count");
+
+let count = 0;
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+    count += 1;
+    countEl.textContent = count;
 }
 
 function decrement() {
-    count -= 1
-    countEl.textContent = count
+    count -= 1;
+    countEl.textContent = count;
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+    let countStr = count + " - ";
+    prevCountEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
 }
