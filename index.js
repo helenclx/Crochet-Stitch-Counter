b98b6f837efdd6e3ab567e8a88a27aec3da94854
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-01-13 14:55:59 +0800

    Refactor button functions to event listeners

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const countEl = document.querySelector(".count");
 const prevCountEl = document.querySelector(".prev-count");
+const incrementBtn = document.querySelector(".increment-btn");
+const decrementBtn = document.querySelector(".decrement-btn");
+const saveBtn = document.querySelector(".save-btn");
 
 let count = 0;
+
+incrementBtn.addEventListener("click", increment);
+decrementBtn.addEventListener("click", decrement);
+saveBtn.addEventListener("click", save);
 
 function increment() {
     count += 1;
